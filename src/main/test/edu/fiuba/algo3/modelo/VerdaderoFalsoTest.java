31b5bbf79d16ceb7a2d2299c7f3342164306e533
Joaquin <joaquinfontela@gmail.com>
2020-07-20 03:26:12 -0300

    falta el test 2

--- src/main/test/edu/fiuba/algo3/modelo/VerdaderoFalsoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/VerdaderoFalsoTest.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,23 +9,45 @@ import java.util.List;
 
 public class VerdaderoFalsoTest {
 
-    @Test
-    public void test01seCreaUnaPreguntaVerdaderoOFalsoYSeVerificaLaOpcionCorrecta() {
+    @BeforeEach
+    void init() {
 
         ModalidadClasica modalidad = new ModalidadClasica();
 
         String enunciado = "El agua hierve a 100 C.";
 
         List<String> opcionesCorrectas = new ArrayList<String>();
-        opcionesCorrectas.add("Verdadero");
+        opcionesCorrectas.add("Verdadero");  // id opcion = 1
 
         List<String> opcionesIncorrectas = new ArrayList<String>();
-        opcionesIncorrectas.add("Falso");
+        opcionesIncorrectas.add("Falso");   // id opcion = 2
 
         VerdaderoFalso pregunta = new VerdaderoFalso(modalidad, enunciado, opcionesCorrectas, opcionesIncorrectas);
+    }
 
+
+    @Test
+    public void test01seCreaUnaPreguntaVerdaderoOFalsoYSeVerificaLaOpcionCorrecta() {
         assertEquals(pregunta.obtenerOpcionesCorrectas().get(0).enunciado(), "Verdadero");
+    }
 
+    @Test
+    public void test02seCreaUnaPreguntaVerdaderoOFalsoYSeVerificaLaCorrectaAsignacionDePuntos() {
+
+        // HACER CON MOCKITO!!!
+
+        Jugador jugador1 = new Jugador("Santiago");
+        Jugador jugador2 = new Jugador("Roberto");
+
+        GestorDeJuego gestor = new GestorDeJuego();
+        gestor.agregarJugador(jugador1);
+        gestor.agregarJugador(jugador2);
+
+        jugador1.setRespuestaADar(new ArrayList<String>(List.of(1)));
+
+        gestor.obtenerRespuesta(jugador1);
+        gestor.obtenerRespuesta(jugador2);
+        
     }
 
 }
