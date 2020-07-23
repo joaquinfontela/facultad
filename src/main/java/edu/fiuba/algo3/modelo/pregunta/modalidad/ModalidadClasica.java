7c37312c2cb98a349c8599ed6f5b9a7fcd13efdb
Nicolás Podesta <npodesta@fi.uba.ar>
2020-07-23 01:45:35 -0300

    Cambio nombre de variable y añado un comentario en ModalidadClasica

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/modalidad/ModalidadClasica.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/modalidad/ModalidadClasica.java
@@ -9,13 +9,16 @@ public class ModalidadClasica extends ModalidadConExclusividad {
 
     @Override
     public HashMap<Integer, Integer> obtenerPuntajesPorJugador(HashMap<Integer, EstadisticasRespuestas> idsJugadores_estadisticasRespuestas){
+
+        //Refactorizar
+
         HashMap<Integer, Integer> puntajes = new HashMap<Integer, Integer>();
-        for (Map.Entry<Integer, EstadisticasRespuestas> iterador : idsJugadores_estadisticasRespuestas.entrySet()) {
-            EstadisticasRespuestas estadisticasActual = iterador.getValue();
+        for (Map.Entry<Integer, EstadisticasRespuestas> entrada : idsJugadores_estadisticasRespuestas.entrySet()) {
+            EstadisticasRespuestas estadisticasActual = entrada.getValue();
             if(estadisticasActual.hayOpcionesIncorrectas() || estadisticasActual.hayOpcionesCorrectasNoElegidas()){
-                puntajes.put(iterador.getKey(), 0);
+                puntajes.put(entrada.getKey(), 0);
             } else{
-                puntajes.put(iterador.getKey(), 1);
+                puntajes.put(entrada.getKey(), 1);
             }
         }
         return puntajes;
