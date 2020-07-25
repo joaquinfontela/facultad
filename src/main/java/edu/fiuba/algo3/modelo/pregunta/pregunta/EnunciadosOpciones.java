dce49d062a3783081a020f0361d9d19532810f73
Joaquin <jbetz@fi.uba.ar>
2020-07-25 04:20:12 -0300

    termino clase enunciadoOpciones

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/pregunta/EnunciadosOpciones.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/pregunta/EnunciadosOpciones.java
@@ -1,12 +1,33 @@
 package edu.fiuba.algo3.modelo.pregunta.pregunta;
 
+import edu.fiuba.algo3.modelo.pregunta.respuesta.Respuesta;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class EnunciadosOpciones {
     HashMap<Integer, ArrayList<String>> opciones;
     public EnunciadosOpciones(){
         opciones=new HashMap<Integer, ArrayList<String>>();
     }
-
+    public ArrayList<String> opcionesCorrectas(){
+        return opciones.get(1);
+    }
+    public ArrayList<String> opcionesIncorrectas(){
+        return opciones.get(0);
+    }
+    public ArrayList<String> opcionesGrupoA(){
+        return opciones.get(0);
+    }
+    public ArrayList<String> opcionesGrupoB(){
+        return opciones.get(1);
+    }
+    public ArrayList<String> opcionesOrdenadas(){
+        ArrayList<String> enunciadosOrdenados = new ArrayList<>();
+        for (Integer i=0; i < opciones.size();i++){
+            enunciadosOrdenados.add(opciones.get(i).get(0));
+        }
+        return enunciadosOrdenados;
+    }
 }
