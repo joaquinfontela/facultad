c2bbbecbebbda39fe70dab90cdac7aac718a9380
Joaquin <joaquinfontela@gmail.com>
2020-07-26 02:57:12 -0300

    hago pasar los dos tests en rtaVoF

--- src/main/test/edu/fiuba/algo3/modelo/RespuestaVerdaderoFalsoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/RespuestaVerdaderoFalsoTest.java
@@ -3,31 +3,63 @@ import edu.fiuba.algo3.modelo.pregunta.pregunta.EnunciadosOpciones;
 import edu.fiuba.algo3.modelo.pregunta.respuesta.EstadisticasRespuestas;
 import edu.fiuba.algo3.modelo.pregunta.respuesta.RespuestaVerdaderoFalso;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class RespuestaVerdaderoFalsoTest {
 
+    EnunciadosOpciones enunciadosOpcionesRespuestaCorrecta;
+    EnunciadosOpciones enunciadosOpcionesRespuestaRecibida;
+    RespuestaVerdaderoFalso respuestaCorrecta;
+    RespuestaVerdaderoFalso respuestaRecibida;
+
+    @BeforeEach
+    public void init() {
+
+        enunciadosOpcionesRespuestaCorrecta = new EnunciadosOpciones();
+        enunciadosOpcionesRespuestaRecibida = new EnunciadosOpciones();
+
+        respuestaCorrecta = new RespuestaVerdaderoFalso();
+        respuestaRecibida = new RespuestaVerdaderoFalso();
+    }
+
     @Test
     public void test01creoDosRespuestasYLasComparoEsperandoObtenerUnaCorrectaElegida() {
-
-        EnunciadosOpciones enunciadosOpcionesRespuestaCorrecta = new EnunciadosOpciones();
-        EnunciadosOpciones enunciadosOpcionesRespuestaRecibida = new EnunciadosOpciones();
 
         enunciadosOpcionesRespuestaCorrecta.agregarEnunciadoEidentificador(0, "Falso");
         enunciadosOpcionesRespuestaCorrecta.agregarEnunciadoEidentificador(1, "Verdadero");
         enunciadosOpcionesRespuestaRecibida.agregarEnunciadoEidentificador(0, "Falso");
         enunciadosOpcionesRespuestaRecibida.agregarEnunciadoEidentificador(1, "Verdadero");
 
-        RespuestaVerdaderoFalso respuestaCorrecta = new RespuestaVerdaderoFalso();
         respuestaCorrecta.rellenar(enunciadosOpcionesRespuestaCorrecta);
-        RespuestaVerdaderoFalso respuestaRecibida = new RespuestaVerdaderoFalso();
         respuestaRecibida.rellenar(enunciadosOpcionesRespuestaRecibida);
 
         EstadisticasRespuestas estadisticasComparacion = respuestaCorrecta.compararCon(respuestaRecibida);
 
         assertEquals(estadisticasComparacion.obtenerOpcionesCorrectasElegidas(), 1);
+        assertFalse(estadisticasComparacion.hayOpcionesCorrectasNoElegidas());
+        assertFalse(estadisticasComparacion.hayOpcionesIncorrectas());
+    }
 
+    @Test
+    public void test02creoDosRespuestasYLasComparoEsperandoObtenerUnaIncorrectaElegida() {
+
+        enunciadosOpcionesRespuestaCorrecta.agregarEnunciadoEidentificador(0, "Verdadero");
+        enunciadosOpcionesRespuestaCorrecta.agregarEnunciadoEidentificador(1, "Falso");
+        enunciadosOpcionesRespuestaRecibida.agregarEnunciadoEidentificador(0, "Falso");
+        enunciadosOpcionesRespuestaRecibida.agregarEnunciadoEidentificador(1, "Verdadero");
+
+        respuestaCorrecta.rellenar(enunciadosOpcionesRespuestaCorrecta);
+        respuestaRecibida.rellenar(enunciadosOpcionesRespuestaRecibida);
+
+        EstadisticasRespuestas estadisticasComparacion = respuestaCorrecta.compararCon(respuestaRecibida);
+
+        assertEquals(estadisticasComparacion.obtenerOpcionesCorrectasElegidas(), 0);
+        assert(estadisticasComparacion.hayOpcionesIncorrectas());
+        assertFalse(estadisticasComparacion.hayOpcionesCorrectasNoElegidas());
     }
 }
+
