896797c4ec8de9183c431df62229c7501d38877c
Joaquin <jbetz@fi.uba.ar>
2020-08-01 02:03:07 -0300

    agrego respuestaOrderedChoice

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/respuesta/RespuestaOrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/respuesta/RespuestaOrderedChoice.java
@@ -2,13 +2,35 @@ package edu.fiuba.algo3.modelo.pregunta.respuesta;
 
 import edu.fiuba.algo3.modelo.pregunta.pregunta.EnunciadosOpciones;
 
+import java.util.ArrayList;
+
 public class RespuestaOrderedChoice implements Respuesta {
 
-    @Override
-    public EstadisticasRespuesta compararCon(Respuesta otraRespuesta){
-        return new EstadisticasRespuesta();
+    ArrayList<Opcion> opcionesOrdenadas;
+
+    public RespuestaOrderedChoice (){
+        opcionesOrdenadas = new ArrayList<Opcion>();
     }
 
     @Override
-    public void rellenar(EnunciadosOpciones enunciadosOpciones) { }
+    public EstadisticasRespuesta compararCon(Respuesta otraRespuesta){
+        EstadisticasRespuesta estadistica = new EstadisticasRespuesta();
+        for(int i=0; i< opcionesOrdenadas.size(); i++){
+            Opcion opcionPropia = opcionesOrdenadas.get(i);
+            Opcion opcionAComparar = ((RespuestaOrderedChoice)otraRespuesta).opcionesOrdenadas.get(i);
+            if(!(opcionPropia.esLaMismaQue(opcionAComparar))){
+                estadistica.sumarIncorrectaElegida();
+                return estadistica;
+            }
+        }
+        estadistica.sumarCorrectaElegida();
+        return estadistica;
+    }
+
+    @Override
+    public void rellenar(EnunciadosOpciones enunciadosOpciones) {
+        for( String enunciado : enunciadosOpciones.opcionesOrdenadas()){
+            opcionesOrdenadas.add(new Opcion(enunciado));
+        }
+    }
 }
--- src/main/test/edu/fiuba/algo3/modelo/OrderedChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/OrderedChoiceTest.java
@@ -1,9 +1,8 @@
-package noImplementadas;
+package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.jugador.Jugador;
 import edu.fiuba.algo3.modelo.pregunta.modalidad.modalidad.Modalidad;
 import edu.fiuba.algo3.modelo.pregunta.modalidad.modalidad.ModalidadClasica;
-import edu.fiuba.algo3.modelo.pregunta.modalidad.bonificacion.ExclusividadDePuntaje;
 import edu.fiuba.algo3.modelo.pregunta.pregunta.EnunciadosOpciones;
 import edu.fiuba.algo3.modelo.pregunta.pregunta.Pregunta;
 import edu.fiuba.algo3.modelo.pregunta.respuesta.Respuesta;
@@ -16,29 +15,17 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class OrderedChoiceConExclusividadTest {
+public class OrderedChoiceTest {
 
     Pregunta pregunta;
-
-    Jugador jugador1;
-    Jugador jugador2;
-
-    RespuestaOrderedChoice respuestaJugador1;
-    RespuestaOrderedChoice respuestaJugador2;
-
-    EnunciadosOpciones opcionesParaAgregarJugador1;
-    EnunciadosOpciones opcionesParaAgregarJugador2;
 
     @BeforeEach
     public void init() {
 
         Modalidad modalidad = new ModalidadClasica();
 
-        ExclusividadDePuntaje exclusividadDePuntaje = new ExclusividadDePuntaje();
-        modalidad.recibirBonificacion(exclusividadDePuntaje);
-
         String enunciado = "Ordenar los siguientes paises segun la cantidad de copas " +
-                "mundiales de futbol ganadas (en orden descendiente).";
+                           "mundiales de futbol ganadas (en orden descendiente).";
 
         Respuesta respuestaCorrecta = new RespuestaOrderedChoice();
 
@@ -53,20 +40,37 @@ public class OrderedChoiceConExclusividadTest {
         respuestaCorrecta.rellenar(opcionesParaAgregar);
 
         pregunta = new Pregunta(modalidad, enunciado, respuestaCorrecta);
-
-        jugador1 = new Jugador("Santiago");
-        jugador2 = new Jugador("Roberto");
-
-        respuestaJugador1 = new RespuestaOrderedChoice();
-        respuestaJugador2 = new RespuestaOrderedChoice();
-
-        opcionesParaAgregarJugador1 = new EnunciadosOpciones();
-        opcionesParaAgregarJugador2 = new EnunciadosOpciones();
-
     }
 
     @Test
-    public void test01seCreaUnaPreguntaOrderedChoiceYSeVerificaLaCorrectaAsignacionDePuntos() {
+    public void test01seCreaUnaPreguntaOrderedChoiceYSeVerificaLaOpcionCorrecta() {
+
+        RespuestaOrderedChoice respuestaCorrectaVerificacion = new RespuestaOrderedChoice();
+
+        EnunciadosOpciones opcionesParaAgregar = new EnunciadosOpciones();
+
+        opcionesParaAgregar.agregarEnunciadoEidentificador(0, "Brasil");
+        opcionesParaAgregar.agregarEnunciadoEidentificador(1, "Alemania");
+        opcionesParaAgregar.agregarEnunciadoEidentificador(2, "Argentina");
+        opcionesParaAgregar.agregarEnunciadoEidentificador(3, "Inglaterra");
+        opcionesParaAgregar.agregarEnunciadoEidentificador(4, "Belgica");
+
+        respuestaCorrectaVerificacion.rellenar(opcionesParaAgregar);
+
+        assertEquals(pregunta.obtenerRespuestaCorrecta().compararCon(respuestaCorrectaVerificacion).obtenerOpcionesCorrectasElegidas(), 1);
+    }
+
+    @Test
+    public void test02seCreaUnaPreguntaOrderedChoiceYSeVerificaLaCorrectaAsignacionDePuntos() {
+
+        Jugador jugador1 = new Jugador("Santiago");
+        Jugador jugador2 = new Jugador("Roberto");
+
+        RespuestaOrderedChoice respuestaJugador1 = new RespuestaOrderedChoice();
+        RespuestaOrderedChoice respuestaJugador2 = new RespuestaOrderedChoice();
+
+        EnunciadosOpciones opcionesParaAgregarJugador1 = new EnunciadosOpciones();
+        EnunciadosOpciones opcionesParaAgregarJugador2 = new EnunciadosOpciones();
 
         opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(0, "Brasil");
         opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(1, "Alemania");
@@ -92,12 +96,21 @@ public class OrderedChoiceConExclusividadTest {
 
         pregunta.evaluarRespuestas(respuestasJugadores);
 
-        assertEquals(jugador1.obtenerPuntaje(), 2);
+        assertEquals(jugador1.obtenerPuntaje(), 1);
         assertEquals(jugador2.obtenerPuntaje(), 0);
     }
 
     @Test
-    public void test02seCreaUnaPreguntaOrderedChoiceYSeVerificaLaCorrectaAsignacionDePuntos() {
+    public void test03seCreaUnaPreguntaOrderedChoiceYSeVerificaLaCorrectaAsignacionDePuntos() {
+
+        Jugador jugador1 = new Jugador("Santiago");
+        Jugador jugador2 = new Jugador("Roberto");
+
+        RespuestaOrderedChoice respuestaJugador1 = new RespuestaOrderedChoice();
+        RespuestaOrderedChoice respuestaJugador2 = new RespuestaOrderedChoice();
+
+        EnunciadosOpciones opcionesParaAgregarJugador1 = new EnunciadosOpciones();
+        EnunciadosOpciones opcionesParaAgregarJugador2 = new EnunciadosOpciones();
 
         opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(0, "Belgica");
         opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(1, "Alemania");
@@ -105,8 +118,8 @@ public class OrderedChoiceConExclusividadTest {
         opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(3, "Inglaterra");
         opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(4, "Brasil");
 
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(0, "Argentina");
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(1, "Alemania");
+        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador (0, "Argentina");
+        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador (1, "Alemania");
         opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(2, "Brasil");
         opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(3, "Inglaterra");
         opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(4, "Belgica");
@@ -125,36 +138,5 @@ public class OrderedChoiceConExclusividadTest {
 
         assertEquals(jugador1.obtenerPuntaje(), 0);
         assertEquals(jugador2.obtenerPuntaje(), 0);
-    }
-
-    @Test
-    public void test03seCreaUnaPreguntaOrderedChoiceYSeVerificaLaCorrectaAsignacionDePuntos() {
-
-        opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(0, "Brasil");
-        opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(1, "Alemania");
-        opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(2, "Argentina");
-        opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(3, "Inglaterra");
-        opcionesParaAgregarJugador1.agregarEnunciadoEidentificador(4, "Belgica");
-
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(0, "Brasil");
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(1, "Alemania");
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(2, "Argentina");
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(3, "Inglaterra");
-        opcionesParaAgregarJugador2.agregarEnunciadoEidentificador(4, "Belgica");
-
-        respuestaJugador1.rellenar(opcionesParaAgregarJugador1);
-        respuestaJugador2.rellenar(opcionesParaAgregarJugador2);
-
-        RespuestaDeJugador respuestaDeJugador1 = new RespuestaDeJugador(jugador1, respuestaJugador1);
-        RespuestaDeJugador respuestaDeJugador2 = new RespuestaDeJugador(jugador2, respuestaJugador2);
-
-        ArrayList<RespuestaDeJugador> respuestasJugadores = new ArrayList<>();
-        respuestasJugadores.add(respuestaDeJugador1);
-        respuestasJugadores.add(respuestaDeJugador2);
-
-        pregunta.evaluarRespuestas(respuestasJugadores);
-
-        assertEquals(jugador1.obtenerPuntaje(), 1);
-        assertEquals(jugador2.obtenerPuntaje(), 1);
     }
 }
