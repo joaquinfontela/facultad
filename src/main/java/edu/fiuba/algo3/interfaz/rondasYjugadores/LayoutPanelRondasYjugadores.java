074125312af0f76db4454848aa132d9a1dc49171
Joaquin <jbetz@fi.uba.ar>
2020-08-09 03:57:54 -0300

    pantalla agregar jugadores y cantidad de rondas

---
+++ src/main/java/edu/fiuba/algo3/interfaz/rondasYjugadores/LayoutPanelRondasYjugadores.java
@@ -0,0 +1,43 @@
+package edu.fiuba.algo3.interfaz.rondasYjugadores;
+
+import edu.fiuba.algo3.interfaz.estilos.EstilosBotonEnviarRespuesta;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class LayoutPanelRondasYjugadores {
+    private BorderPane layout;
+    private TextField espacioJugador1;
+    private TextField espacioJugador2;
+    private Label jugador1;
+    private Label jugador2;
+    private Label rondas;
+    private Button cincoRondas;
+    private Button diezRondas;
+    private Button quinceRondas;
+    private Button enviarRespuesta;
+
+
+    public LayoutPanelRondasYjugadores() {
+        layout = new BorderPane();
+        layout.setMaxSize(1500, 900);
+        layout.setMinSize(1500, 900);
+
+        rondas = new Label("Rondas: ");
+        cincoRondas = new Button("5");
+        diezRondas = new Button("10");
+        quinceRondas = new Button("15");
+
+        jugador1 = new Label("Jugador 1: ");
+        jugador2 = new Label("Jugador 2: ");
+        espacioJugador1 = new TextField();
+        espacioJugador2 = new TextField();
+
+        enviarRespuesta = new Button("Jugar");
+        enviarRespuesta.setSkin(new EstilosBotonEnviarRespuesta(enviarRespuesta));
+
+        layout.getChildren().addAll();
+
+    }
+}
