1951bd423be0910f95a294347066a0e0b4f21e0c
ncrypto <npodesta@fi.uba.ar>
2020-08-13 15:56:29 -0300

    Agrego controlador en el boton de LayoutPreturno

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -23,23 +23,22 @@ public class App extends Application {
 
         //System.out.println(javafx.scene.text.Font.getFamilies());
 
-        //mostrarLayoutPregunta();
         //mostrarLayoutPuntajesParciales();
         //mostrarLayoutPuntajeFinal();
-        //mostrarLayoutInicio(stage);
-        //mostrarLayoutPreturno();
-        //mostrarLayoutRegistro();
 
         layoutPregunta = new LayoutPregunta(7, 10);
 
-        layoutPregunta.agregarEnunciadoDeLaPregunta("Que seleccion es la mas ganadora de la historia de los mundiales? (seleccionar mas de una en caso de ser necesario)");
+        layoutPregunta.agregarEnunciadoDeLaPregunta("Que seleccion es la mas ganadora de la historia de los mundiales? " +
+                "(seleccionar mas de una en caso de ser necesario)");
 
         layoutPregunta.agregarOpcion("Italia");
         layoutPregunta.agregarOpcion("Brasil");
         layoutPregunta.agregarOpcion("Argentina");
         layoutPregunta.agregarOpcion("Alemania");
 
-        layoutRegistro = new LayoutRegistro(stage, new Scene(layoutPregunta.getLayout(), 640, 480));
+        layoutPreturno = new LayoutPreturno(stage, new Scene(layoutPregunta.getLayout(), 640, 480),
+                "LOLITOFDEZ");
+        layoutRegistro = new LayoutRegistro(stage, new Scene(layoutPreturno, 640, 480));
         layoutInicio = new LayoutInicio(stage, new Scene(layoutRegistro, 640, 480));
         scene = new Scene(layoutInicio, 640, 480);
 
@@ -53,22 +52,6 @@ public class App extends Application {
         stage.setHeight(720);
         stage.setWidth(1200);
         stage.show();
-    }
-
-    private void mostrarLayoutPregunta() {
-
-        layoutPregunta = new LayoutPregunta(7, 10);
-
-        layoutPregunta.agregarEnunciadoDeLaPregunta("Que seleccion es la mas ganadora de la historia de los mundiales? (seleccionar mas de una en caso de ser necesario)");
-
-        layoutPregunta.agregarOpcion("Italia");
-        layoutPregunta.agregarOpcion("Brasil");
-        layoutPregunta.agregarOpcion("Argentina");
-        layoutPregunta.agregarOpcion("Alemania");
-        //layoutPregunta.agregarOpcion("Holanda");
-        //layoutPregunta.agregarOpcion("Uruguay");
-
-        scene = new Scene(layoutPregunta.getLayout(), 640, 480);
     }
 
     private void mostrarLayoutPuntajesParciales() {
@@ -88,25 +71,6 @@ public class App extends Application {
 
         scene = new Scene(layoutPuntajeFinal.getLayout(), 640, 480);
     }
-    /*
-    public void mostrarLayoutInicio(Stage stage) {
-
-        layoutRegistro = new LayoutRegistro();
-        layoutInicio = new LayoutInicio(stage, new Scene(layoutRegistro, 640, 480));
-        scene = new Scene(layoutInicio, 640, 480);
-    }*/
-
-    private void mostrarLayoutPreturno() {
-
-        layoutPreturno = new LayoutPreturno("miguelito123");
-        scene = new Scene(layoutPreturno, 640, 480);
-    }
-    /*
-    private void mostrarLayoutRegistro() {
-
-        layoutRegistro = new LayoutRegistro();
-        scene = new Scene(layoutRegistro, 640, 480);
-    }*/
 
     public static void main(String[] args) {
         launch();
--- src/main/java/edu/fiuba/algo3/interfaz/layouts/LayoutPreturno.java
+++ src/main/java/edu/fiuba/algo3/interfaz/layouts/LayoutPreturno.java
@@ -1,7 +1,9 @@
 package edu.fiuba.algo3.interfaz.layouts;
 
+import edu.fiuba.algo3.controladores.BotonCambiarPantallaHandler;
 import edu.fiuba.algo3.interfaz.botones.BotonContinuar;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
@@ -10,13 +12,11 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class LayoutPreturno extends StackPane {
 
-    private BotonContinuar botonContinuar;
-    private Label cartelProximoTurno;
-
-    public LayoutPreturno(String nicknameJugadorProximoTurno) {
+    public LayoutPreturno(Stage stage, Scene proximaEscena, String nicknameJugadorProximoTurno) {
 
         this.setBackground(new Background(new BackgroundFill(Color.PURPLE, CornerRadii.EMPTY, Insets.EMPTY)));
 
@@ -27,15 +27,16 @@ public class LayoutPreturno extends StackPane {
         textArea.setStyle("-fx-border-color: mediumpurple; -fx-background-color: transparent; -fx-border-width: 5px");
         this.getChildren().add(textArea);
 
-        agregarBotonContinuar();
+        agregarBotonContinuar(stage, proximaEscena);
         agregarTitulo();
         agregarNicknameProximoTurno(nicknameJugadorProximoTurno);
     }
 
-    private void agregarBotonContinuar() {
+    private void agregarBotonContinuar(Stage stage, Scene proximaEscena) {
 
-        botonContinuar = new BotonContinuar();
+        BotonContinuar botonContinuar = new BotonContinuar();
         botonContinuar.setTranslateY(200.0);
+        botonContinuar.setOnAction(new BotonCambiarPantallaHandler(stage, proximaEscena));
         this.getChildren().add(botonContinuar);
     }
 
@@ -50,7 +51,7 @@ public class LayoutPreturno extends StackPane {
 
     private void agregarNicknameProximoTurno(String nicknameJugadorProximoTurno) {
 
-        cartelProximoTurno = new Label();
+        Label cartelProximoTurno = new Label();
         cartelProximoTurno.setTranslateY(-50.0);
         cartelProximoTurno.setText(nicknameJugadorProximoTurno);
         cartelProximoTurno.setFont(new Font("KacstPoster", 95));
