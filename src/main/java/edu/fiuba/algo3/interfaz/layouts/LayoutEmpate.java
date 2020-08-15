f5affbc139a45dc85d027bced1a02d80860fe2f5
Agustin <agusghersi@gmail.com>
2020-08-14 23:06:09 -0300

    Arreglo errores menores en LayoutEmpate

--- src/main/java/edu/fiuba/algo3/interfaz/layouts/LayoutEmpate.java
+++ src/main/java/edu/fiuba/algo3/interfaz/layouts/LayoutEmpate.java
@@ -1,5 +1,4 @@
 package edu.fiuba.algo3.interfaz.layouts;
-
 import edu.fiuba.algo3.interfaz.estilos.Tic;
 import edu.fiuba.algo3.interfaz.layouts.puntajesSubLayouts.LayoutPuntajeJugador;
 import edu.fiuba.algo3.modelo.jugador.Jugador;
@@ -8,68 +7,57 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
-public class LayoutEmpate {
-        package edu.fiuba.algo3.interfaz.layouts;
+public class LayoutEmpate extends StackPane {
 
-import edu.fiuba.algo3.interfaz.estilos.Tic;
-import edu.fiuba.algo3.interfaz.layouts.puntajesSubLayouts.LayoutPuntajeJugador;
-import edu.fiuba.algo3.modelo.jugador.Jugador;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
+    public LayoutEmpate(Jugador jugadorArriba, Jugador jugadorAbajo) {
 
-    public class LayoutEmpate extends StackPane {
-
-        public LayoutEmpate(Jugador jugadorGanador, Jugador jugadorPerdedor) {
-
-            ImageView imageView = new ImageView();
-            imageView.setImage(new Image("file:src/imagenes/fondo.jpg"));
-            imageView.setFitHeight(760);
-            imageView.setFitWidth(1300);
-            this.getChildren().add(imageView);
-            crearLayoutParteJugadorArriba(jugadorGanador);
-            crearLayoutParteJugadorAbajo(jugadorPerdedor);
-        }
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("file:src/imagenes/fondo.jpg"));
+        imageView.setFitHeight(760);
+        imageView.setFitWidth(1300);
+        this.getChildren().add(imageView);
+        crearLayoutParteJugadorArriba(jugadorArriba);
+        crearLayoutParteJugadorAbajo(jugadorAbajo);
+    }
 
 
-        private void crearLayoutParteJugadorArriba(Jugador jugadorArriba) {
+    private void crearLayoutParteJugadorArriba(Jugador jugadorArriba) {
 
-            LayoutPuntajeJugador layoutPuntajeArriba = new LayoutPuntajeJugador(jugadorArriba, Color.LIMEGREEN);
-            layoutPuntajeArriba.setTranslateY(-120.0);
+        LayoutPuntajeJugador layoutPuntajeArriba = new LayoutPuntajeJugador(jugadorArriba, Color.LIMEGREEN);
+        layoutPuntajeArriba.setTranslateY(-120.0);
 
-            Tic tic = new Tic();
-            tic.setScaleX(20);
-            tic.setScaleY(20);
-            tic.setFill(Color.DARKGREEN);
-            tic.setTranslateX(650.0);
-            layoutPuntajeArriba.getChildren().add(tic);
+        Tic tic = new Tic();
+        tic.setScaleX(20);
+        tic.setScaleY(20);
+        tic.setFill(Color.DARKGREEN);
+        tic.setTranslateX(650.0);
+        layoutPuntajeArriba.getChildren().add(tic);
 
-            this.getChildren().add(layoutPuntajeArriba);
+        this.getChildren().add(layoutPuntajeArriba);
 
-            ImageView imageView = new ImageView();
-            imageView.setImage(new Image("file:src/imagenes/Empate.jpeg"));
-            imageView.setFitHeight(100);
-            imageView.setFitWidth(100);
-            imageView.setTranslateX(150);
-            imageView.setTranslateY(-120);
-            this.getChildren().add(imageView);
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("file:src/imagenes/Empate.jpeg"));
+        imageView.setFitHeight(100);
+        imageView.setFitWidth(100);
+        imageView.setTranslateX(150);
+        imageView.setTranslateY(-120);
+        this.getChildren().add(imageView);
 
-        }
+    }
 
-        private void crearLayoutParteJugadorAbajo(Jugador jugadorAbajo) {
+    private void crearLayoutParteJugadorAbajo(Jugador jugadorAbajo) {
 
-            LayoutPuntajeJugador layoutPuntajeAbajo = new LayoutPuntajeJugador(jugadorAbajo, Color.DARKRED);
-            layoutPuntajeAbajo.setTranslateY(150.0);
-            this.getChildren().add(layoutPuntajeAbajo);
+        LayoutPuntajeJugador layoutPuntajeAbajo = new LayoutPuntajeJugador(jugadorAbajo, Color.DARKRED);
+        layoutPuntajeAbajo.setTranslateY(150.0);
+        this.getChildren().add(layoutPuntajeAbajo);
 
-            ImageView imageView = new ImageView();
-            imageView.setImage(new Image("file:src/imagenes/Empate.jpeg"));
-            imageView.setFitHeight(100);
-            imageView.setFitWidth(100);
-            imageView.setTranslateX(150);
-            imageView.setTranslateY(150);
-            this.getChildren().add(imageView);
-        }
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("file:src/imagenes/Empate.jpeg"));
+        imageView.setFitHeight(100);
+        imageView.setFitWidth(100);
+        imageView.setTranslateX(150);
+        imageView.setTranslateY(150);
+        this.getChildren().add(imageView);
     }
 }
+
