a6886f4cd57057e43a93107d405a49b9638d84ae
Joaquin <joaquinfontela@gmail.com>
2020-08-16 00:43:09 -0300

    arreglando agadas

--- src/main/java/edu/fiuba/algo3/interfaz/botones/botonesOpcion/BotonOpcion.java
+++ src/main/java/edu/fiuba/algo3/interfaz/botones/botonesOpcion/BotonOpcion.java
@@ -1,13 +1,13 @@
 package edu.fiuba.algo3.interfaz.botones.botonesOpcion;
 
 import edu.fiuba.algo3.interfaz.botones.Boton;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 
 public abstract class BotonOpcion extends Boton {
 
     public BotonOpcion(String enunciado, double posX, double posY, Color color) {
 
-        super();
         this.setText(enunciado);
         this.setTranslateX(posX);
         this.setTranslateY(posY);
