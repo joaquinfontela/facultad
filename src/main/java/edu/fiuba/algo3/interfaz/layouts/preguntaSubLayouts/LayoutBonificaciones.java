0b13b51041b120e2b18c488e9a2dee11422cd988
ncrypto <npodesta@fi.uba.ar>
2020-08-15 21:57:26 -0300

    Pequeño cambio

--- src/main/java/edu/fiuba/algo3/interfaz/layouts/preguntaSubLayouts/LayoutBonificaciones.java
+++ src/main/java/edu/fiuba/algo3/interfaz/layouts/preguntaSubLayouts/LayoutBonificaciones.java
@@ -12,19 +12,14 @@ public class LayoutBonificaciones extends StackPane {
     public LayoutBonificaciones(GestorDeJuego gestor) {
 
         BotonExclusividad botonExclusividad = new BotonExclusividad(-50,-420);
-        if (!gestor.jugadorActualTieneAlgunaExclusividad()) {
-            botonExclusividad.setDisable(true);
-        }
+        if (!gestor.jugadorActualTieneAlgunaExclusividad()) botonExclusividad.setDisable(true);
         this.getChildren().add(botonExclusividad);
         BotonMultiplicadorX2 botonMultiplicadorX2 = new BotonMultiplicadorX2(-50, -285);
-        if (!gestor.jugadorActualTieneAlgunMultiplicadorX2()) {
-            botonExclusividad.setDisable(true);
-        }
+        if (!gestor.jugadorActualTieneAlgunMultiplicadorX2()) botonExclusividad.setDisable(true);
         this.getChildren().add(botonMultiplicadorX2);
         BotonMultiplicadorX3 botonMultiplicadorX3 = new BotonMultiplicadorX3(-50, -170);
-        if (!gestor.jugadorActualTieneAlgunMultiplicadorX3()) {
-            botonExclusividad.setDisable(true);
-        }
+        if (!gestor.jugadorActualTieneAlgunMultiplicadorX3()) botonExclusividad.setDisable(true);
+
         this.getChildren().add(botonMultiplicadorX3);
     }
 }
