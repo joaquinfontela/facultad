85c2dccd29872f5585be81779c0530d6043743e5
ncrypto <npodesta@fi.uba.ar>
2020-08-15 21:32:59 -0300

    Cambio minimo

--- src/main/java/edu/fiuba/algo3/modelo/GestorDeJuego.java
+++ src/main/java/edu/fiuba/algo3/modelo/GestorDeJuego.java
@@ -132,7 +132,6 @@ public class GestorDeJuego {
 
     public Jugador obtenerPosibleJugadorGanador() {
 
-        int contador = 0;
         Jugador candidatoGanador = jugadoresRegistrados.get(0);
         for (Jugador jugador : jugadoresRegistrados) {
             if(jugador.obtenerPuntaje() > candidatoGanador.obtenerPuntaje()) {
