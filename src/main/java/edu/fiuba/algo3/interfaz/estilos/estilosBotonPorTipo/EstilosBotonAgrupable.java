3e04751199dfa5d2d82fc64dc68753d4f8d8161f
Joaquin <joaquinfontela@gmail.com>
2020-08-18 22:49:59 -0300

    arreglo en vista group

--- src/main/java/edu/fiuba/algo3/interfaz/estilos/estilosBotonPorTipo/EstilosBotonAgrupable.java
+++ src/main/java/edu/fiuba/algo3/interfaz/estilos/estilosBotonPorTipo/EstilosBotonAgrupable.java
@@ -3,6 +3,11 @@ package edu.fiuba.algo3.interfaz.estilos.estilosBotonPorTipo;
 import edu.fiuba.algo3.interfaz.botones.Boton;
 import edu.fiuba.algo3.interfaz.botones.tipoBoton.Agrupable;
 import javafx.animation.FadeTransition;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 public class EstilosBotonAgrupable implements EstilosBotonPorTipo {
@@ -13,6 +18,8 @@ public class EstilosBotonAgrupable implements EstilosBotonPorTipo {
     public void aplicarEstilos(Boton unBoton) {
 
         boton = unBoton;
+        boton.setBackground(new Background(new BackgroundFill(Color.web("d8b97b"), CornerRadii.EMPTY, Insets.EMPTY)));
+        boton.setTextFill(Color.BLACK);
         agrupable = (Agrupable) boton.getTipo();
 
         eventoMousePasaPorArriba();
