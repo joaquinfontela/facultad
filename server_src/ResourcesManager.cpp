5c3132586445f119b64e416904a2bfbb54a36a37
Joaquin <joaquinfontela@gmail.com>
2020-11-18 01:37:18 -0300

    agrego locks faltantes

--- server_src/ResourcesManager.cpp
+++ server_src/ResourcesManager.cpp
@@ -3,10 +3,12 @@
 ResourcesManager::ResourcesManager() {}
 
 void ResourcesManager::addResource(std::string name, std::string body) {
+  std::unique_lock<std::mutex> lock(m);
   resources.insert({name, body});
 }
 
 bool ResourcesManager::hasResource(const std::string& resourceName) {
+  std::unique_lock<std::mutex> lock(m);
   return (resources.find(resourceName) != resources.end());
 }
 
