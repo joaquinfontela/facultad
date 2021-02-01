9aa0c9c9682813afd590c1a8cdbd9a1af407c0a7
JDR <jrosenblatt@fi.uba.ar>
2021-02-01 02:14:27 -0300

    Mejorando textos

--- src/bot.ts
+++ src/bot.ts
@@ -15,8 +15,8 @@ export class Bot {
 
     private initializeCommands() {
         this.commandList.push(new Command("Brinda información de los comandos disponibles.", "ayuda"));
-        this.commandList.push(new Command("Desconecta al bot como si te piden que actives la cámara", "tomatela"));
-        this.commandList.push(new Command("Brinda una lista de materias que se pueden cursar, en función de los códigos de materias aprobados dados", "disponibles"));
+        this.commandList.push(new Command("Desconecta al bot como, si te obligaran a activar la cámara.", "tomatela"));
+        this.commandList.push(new Command("Brinda una lista de materias que se pueden cursar, en función de los códigos de materias aprobadas dadas", "disponibles"));
         this.commandList.push(new Command("Muestra que materias uno necesita tener aprobado para cursar una en particular", "restantes"));
         this.commandList.push(new Command("Responde con la cantidad de créditos (robados) conseguidos", "creds"));
         this.commandList.push(new Command("Guarda en el sistema las materias aprobadas", "aprobe"));
