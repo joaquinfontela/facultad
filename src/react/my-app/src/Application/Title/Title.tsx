40dcd718391a523ce0bf6008b3e801bc68f1a9d7
Joaquin Fontela <jfontela@fi.uba.ar>
2021-02-12 19:55:14 -0300

    refactoring class Title

---
+++ src/react/my-app/src/Application/Title/Title.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Title.css';
+
+export function Title(props: Object) {
+    return (
+        <h1 className="title">
+            FIUBA-Bot
+        </h1>
+    );
+}
